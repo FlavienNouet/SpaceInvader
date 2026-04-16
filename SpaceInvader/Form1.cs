04d6b5b809c61c546f460e58f4cc2b93cbb07de7
unknown <flavien-nouet@outlook.fr>
2026-04-16 09:05:48 +0200

    test affichage vaisseau v1

--- SpaceInvader/Form1.cs
+++ SpaceInvader/Form1.cs
@@ -2,8 +2,36 @@ namespace SpaceInvader;
 
 public partial class Form1 : Form
 {
+    private readonly Game game;
+    private readonly System.Windows.Forms.Timer frameTimer = new();
+    private DateTime lastFrameUtc;
+
     public Form1()
     {
         InitializeComponent();
+        DoubleBuffered = true;
+
+        game = new Game(ClientSize);
+        lastFrameUtc = DateTime.UtcNow;
+
+        frameTimer.Interval = 16;
+        frameTimer.Tick += FrameTimer_Tick;
+        frameTimer.Start();
+    }
+
+    protected override void OnPaint(PaintEventArgs e)
+    {
+        base.OnPaint(e);
+        game.Draw(e.Graphics);
+    }
+
+    private void FrameTimer_Tick(object? sender, EventArgs e)
+    {
+        DateTime now = DateTime.UtcNow;
+        double deltaTimeSeconds = (now - lastFrameUtc).TotalSeconds;
+        lastFrameUtc = now;
+
+        game.Update(deltaTimeSeconds);
+        Invalidate();
     }
 }
